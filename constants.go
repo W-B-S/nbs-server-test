838d703557f60c0e14bddaf422ca09845ebb5d0b
W-B-S <lws99927800@163.com>
2018-06-29 16:30:26 +0800

    bug fixed for tips

--- constants.go
+++ constants.go
@@ -3,4 +3,12 @@ package main
 
 const(
 	logFileName = "NBS-Server.log"
+	EnvEnableProfiling = "IPFS_PROF"
+	cpuProfile         = "ipfs.cpuprof"
+	heapProfile        = "ipfs.memprof"
+	cmdActionTypeToInit	  = "1"
+	cmdActionTypeToStart  = "2"
+	cmdActionTypeRunning  = "3"
+	cmdActionTypeError    = "4"
+	ipfsConnectionUrl  = "http://127.0.0.1:5001/webui/"
 )
--- main.go
+++ main.go
@@ -49,17 +49,6 @@ var format = logging.MustStringFormatter(
 
 var errRequestCanceled = errors.New("request canceled")
 
-const (
-	EnvEnableProfiling = "IPFS_PROF"
-	cpuProfile         = "ipfs.cpuprof"
-	heapProfile        = "ipfs.memprof"
-	cmdActionTypeToInit	  = "1"
-	cmdActionTypeToStart  = "2"
-	cmdActionTypeRunning  = "3"
-	cmdActionTypeError    = "4"
-	ipfsConnectionUrl  = "http://127.0.0.1:5001/webui/"
-)
-
 // main roadmap:
 // - parse the commandline to get a cmdInvocation
 // - if user requests help, print it and exit.
